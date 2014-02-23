646da4a09839c33b791bf97f991a34bbff563edf
Ross Mills <ross_mills1@hotmail.co.uk>
2014-02-23 21:20:04 +0000

    add rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Therapy::Application.config.secret_key_base = '2f4573a708b925e2563752c23692693ea9d5a08c269f7d8830a1d9f91d2880d845cb37433edd31b9f5c7c244828d282d1e3322926b564837f5f91af4395eb89e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Therapy::Application.config.session_store :cookie_store, key: '_therapy_session'
